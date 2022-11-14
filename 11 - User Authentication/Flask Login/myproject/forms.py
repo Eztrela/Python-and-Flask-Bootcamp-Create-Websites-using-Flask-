76236e0c0a7bbf49527df68ba33619e7f3cedca9
Eztrela <pablolm.estrela@gmail.com>
2022-11-14 11:59:07 -0300

    adicionando mais arquivos de user Authentication

--- 11 - User Authentication/Flask Login/myproject/forms.py
+++ 11 - User Authentication/Flask Login/myproject/forms.py
@@ -2,6 +2,8 @@ from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordField,SubmitField
 from wtforms.validators import DataRequired,Email,EqualTo
 from wtforms import ValidationError
+from myproject.models import User
+
 
 
 class LoginForm(FlaskForm):
@@ -10,17 +12,15 @@ class LoginForm(FlaskForm):
   submit = SubmitField('Log in')
 
 
-class RegistratuionForm(FlaskForm):
+class RegistrationForm(FlaskForm):
   email = StringField('Email',validators=[DataRequired(),Email()])
   username = StringField('Username',validators=[DataRequired()])
   password = PasswordField('Password',validators=[DataRequired(),EqualTo('pass_confirm', message='Password must match')])
   pass_confirm = PasswordField('Confirm Password',validators=[DataRequired()])
   submit = SubmitField('Register')
-  def check_email(self,field):
-    if User.query.filter_by(email = field.data).first():
-      raise ValidationError('Your email has been already registered!')
-  
-  def check_username(self,field):
-    if User.query.filter_by(username = field.data).first():
-      raise ValidationError('Username is Taken')
-  
+  def validate_email(self, email):
+        if User.query.filter_by(email=self.email.data).first():
+            raise ValidationError('Email has been registered')
+  def validate_username(self, username):
+      if User.query.filter_by(username=self.username.data).first():
+          raise ValidationError('Username has been registered')
--- 11 - User Authentication/Flask Login/myproject/models.py
+++ 11 - User Authentication/Flask Login/myproject/models.py
@@ -10,16 +10,16 @@ def load_user(user_id):
 class User(db.Model,UserMixin):
   __tablename__ = 'users'
 
-  id = db.Column(db.Integer,primary_key = True)
-  email = db.Column(db.String(64), unique =  True, index = True)
-  username = db.Column(db.String(64), unique =  True, index = True)
+  id = db.Column(db.Integer,primary_key=True)
+  email = db.Column(db.String(64), unique=True, index=True)
+  username = db.Column(db.String(64), unique=True, index=True)
   password_hash = db.Column(db.String(128))
 
   def __init__(self,email,username,password) -> None:
     self.email = email
     self.username = username
-    self.password_hash = generate_password_hash(password=password)
+    self.password_hash = generate_password_hash(password)
 
   def check_password(self,password):
-    return check_password_hash(self.password_hash,password = password)
+    return check_password_hash(self.password_hash,password)
   
